68c69b270400d7e956f7089876a9460d6465ad70
Ajay Singh <ajaysngh040@gmail.com>
2024-11-08 15:08:41 +0530

    checkIsAuthenticated modified

--- src/lib/auth/checkIsAuthenticated.ts
+++ src/lib/auth/checkIsAuthenticated.ts
@@ -7,7 +7,7 @@ import { auth } from "./authOptions";
 export async function checkIsAuthenticated() {
   const session = await auth();
   if(session){
-    return session ? true : false
+    return  !!session;
   }
   
 }
