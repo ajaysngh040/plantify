f2a597cbd417e38a877286a3f59eddea9612725e
Ajay Singh <ajaysngh040@gmail.com>
2024-11-08 16:54:40 +0530

    nextauth secret updated

--- src/lib/auth/authOptions.ts
+++ src/lib/auth/authOptions.ts
@@ -7,7 +7,7 @@ import Google from "next-auth/providers/google"
 
 export  const {handlers,signIn,signOut,auth} = NextAuth({
   adapter: PrismaAdapter(prisma),
-  secret:process.env.AUTH_SECRET,
+  secret:process.env.NEXTAUTH_SECRET,
   providers: [
     Google({
     clientId:process.env.GOOGLE_CLIENT_ID,
